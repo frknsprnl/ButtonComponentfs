5d700fbdc6aa305660eb93d74f65502870adb3d7
Furkan <frknsprnl@hotmail.com>
2022-10-12 02:36:56 +0300

    button component

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
-import { ExampleComponent } from 'buttoncomponentfs'
+import {  Button } from 'buttoncomponentfs'
 import 'buttoncomponentfs/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return ( 
+  <>
+    <div style={{height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+      <Button text="Click me" type="primary" />
+      <Button text="Click me" />
+      <Button text="Click me" type="dashed" />
+      <Button text="Click me" type="text" />
+      <Button text="Click me" type="link" />
+      <Button text="Click me" type="danger" />
+    </div>
+  </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ import styles from './styles.module.css'
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+export const Button = (props) => {
+  return (
+  <button {...props} className={`${styles.button} ${props.type ? styles[props.type] : styles.button}` } >{props.text}</button>
+  )
+}
